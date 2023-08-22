c50d41b7ce518adbbef977aaf2b3b3a6669c4730
HallZero <filipikikuchi@gmail.com>
2023-08-22 16:27:41 -0300

    feat(front): :sparkles: Add id from front

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from motor.motor_asyncio import AsyncIOMotorClient
 import httpx
 
 app = FastAPI()
+
+# Templates
+templates = Jinja2Templates(directory="templates")
 
 # Connect to MongoDB using motor
 mongo_uri = "mongodb+srv://jvDev:rLhvxX5wWVPoZw40@clusternave.2pqpbpv.mongodb.net/?retryWrites=true&w=majority"
@@ -11,11 +16,12 @@ db = client["ClusterNave"]
 collection = db["elite6-data"]
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, 'data': None})
 
 @app.get('/setDB/{id}')
 async def setDB(id: int, request: Request):
+    print(id)
     url = f'https://pokeapi.co/api/v2/pokemon/{id}'
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
@@ -28,7 +34,7 @@ async def setDB(id: int, request: Request):
             try:
                 # Insert the fetched data into the MongoDB collection asynchronously
                 await collection.insert_one(pokemon_data)
-                return {'message': 'Data inserted successfully'}
+                return templates.TemplateResponse("index.html", {"request": request, 'data': pokemon_data})
             except Exception as e:
                 return {'error': 'Failed to insert data into the database'}
         else:
@@ -40,6 +46,14 @@ async def show_poke(id: str):
     if pokemon:
         return pokemon
     raise HTTPException(status_code=404, detail=f"Pokémon {id} not found")
+
+# route to get the pokemon id from the index.html form
+@app.post("/pokemon")
+async def get_pokemon(request: Request):
+    form_data = await request.form()
+    pokemon_id = form_data["pokemon_id"]
+    return RedirectResponse(url=str("/setDB/" + pokemon_id))
+
 
 if __name__ == "__main__":
     import uvicorn
